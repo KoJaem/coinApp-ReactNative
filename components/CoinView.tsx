7cfe0e02b1a965a9d34d3f15626612bf40b6e67c
KoJaem <woalswhwh@naver.com>
2022-06-06 00:32:40 +0900

    feat : 무한 스크롤 구현 및 refresh 시 스크롤 초기화 구현

--- components/CoinView.tsx
+++ components/CoinView.tsx
@@ -20,11 +20,20 @@ export function CoinView() {
   const [refreshing, setRefreshing] = useState<boolean>(false);
   const [limit, setLimit] = useState(0);
 
+  const apiLimit = useCallback(() => {
+    const coinArray = coinDataName.slice(limit, limit + 12).join(',');
+    setLimit(limit + 12);
+    return coinArray;
+  }, [limit]);
+
   const getCoinData = useCallback(async () => {
     // setIsLoading(true);
+    if (limit >= coinDataName.length) {
+      return;
+    }
     try {
       const response = await axios.get(
-        `https://api.upbit.com/v1/ticker?markets=${coinDataName.join(',')}`,
+        `https://api.upbit.com/v1/ticker?markets=${apiLimit()}`,
       );
       dispatch(refresh());
       setCoinData(coinData.concat(response.data));
@@ -32,7 +41,7 @@ export function CoinView() {
     } catch (error) {
       console.error('getCoinData', error);
     }
-  }, [coinData, dispatch]);
+  }, [apiLimit, coinData, dispatch, limit]);
 
   useEffect(() => {
     getCoinData();
@@ -48,6 +57,35 @@ export function CoinView() {
       />
     );
   };
+  const moreInfo = () => {
+    setLimit(limit + 12);
+    getCoinData();
+  };
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    RefreshGetData();
+    // setLimit(0), setCoinData([]) 을 하고 getCoinData 호출하는것이 아닌 위 코드를 따로 만든 이유는
+    // 저 방법대로 하면 setUseState 가 두번 호출되는데, 그럼 마지막에 호출된것만 인식이 되기때문에
+    // Refresh 를 했는데 그냥 아래에서 끌어오는것처럼 되기 때문임.
+    setRefreshing(false);
+  };
+
+  const RefreshGetData = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.upbit.com/v1/ticker?markets=${coinDataName
+          .slice(0, 12)
+          .join(',')}`,
+      );
+      dispatch(refresh());
+      setCoinData(response.data);
+      setLimit(12);
+      // setIsLoading(false);
+    } catch (error) {
+      console.error('getCoinData', error);
+    }
+  };
 
   return (
     <FlatList
@@ -55,7 +93,9 @@ export function CoinView() {
       renderItem={renderItem}
       keyExtractor={(item: DataType) => item.market}
       refreshing={refreshing}
-      onRefresh={getCoinData}
+      onRefresh={onRefresh}
+      onEndReached={moreInfo}
+      onEndReachedThreshold={0.2}
     />
   );
 }
