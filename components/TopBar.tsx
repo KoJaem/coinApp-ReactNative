61c8fd95fc90443ad9b31f2208d420deffff5fe1
KoJaem <woalswhwh@naver.com>
2022-06-16 10:38:53 +0900

    feat : Left 버튼 아이콘으로 변경 및 Right 버튼 삭제

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,8 +1,9 @@
-import {Text} from 'react-native';
 import React from 'react';
 import styled from 'styled-components/native';
 import {useTimeSelector} from '../hooks/timeHook';
 import {useNavigation} from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/AntDesign';
+import {TouchableOpacity} from 'react-native';
 
 type Props = {
   title: string;
@@ -13,17 +14,16 @@ export function TopBar({title}: Props) {
   const navigation = useNavigation();
   return (
     <Container>
-      <Text
+      <TouchableOpacity
         onPress={() => {
           navigation.goBack();
         }}>
-        Left
-      </Text>
+        <Icon name="arrowleft" size={24} color="#c82ec5" />
+      </TouchableOpacity>
       <TitleWrapper>
         <Title>{title}</Title>
         <Time>{time}</Time>
       </TitleWrapper>
-      <Text>Right</Text>
     </Container>
   );
 }
@@ -33,12 +33,14 @@ const Container = styled.View`
   height: 52px;
   background-color: #94d2ff;
   align-items: center;
-  justify-content: space-between;
   padding: 0 10px;
 `;
-
 const TitleWrapper = styled.View`
+  position: absolute;
+  left: 0;
+  right: 0;
   align-items: center;
+  z-index: -1; // 이유 : Icon onPress 이벤트를 TitleWrapper 가 가림.
 `;
 
 const Title = styled.Text`
